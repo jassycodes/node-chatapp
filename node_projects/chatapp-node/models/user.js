50f8ecccce233820909166d5bd3f19b5f91536ab
Jasmine <jassycodes@gmail.com>
2019-02-27 20:29:10 -0800

    update chatapp-node

--- node_projects/chatapp-node/models/user.js
+++ node_projects/chatapp-node/models/user.js
@@ -2,29 +2,76 @@
 var db = require('../db/config/config');
 
 module.exports = {
-	// Return a new promise for finding a single user
-	findOne: function (user) {
-		console.log("findOne")
-		console.log(user)
-		console.log(user.username)
-		return new Promise ((resolve, reject) => {
-			const queryString = 'SELECT * FROM Users WHERE username=?';
-			db.query(queryString, [user.username], (err, res) => {
-				console.log(err, res, "hello");
-				if (err) { 
-					// send back an error
-					console.log("found an error");
-					reject(err);
-				} else {
-					if (res.length) {
-						// found a user with username that was passed in
-						resolve(res[0]);
-					} else {
-						// did not find a user with username
-						resolve(false);
-					}
-				}
-			});
-		});
-	},
+    // Return a new promise for finding a single user
+    findOne: function(user) {
+        console.log("findOne")
+        console.log(user)
+        console.log(user.username)
+        return new Promise((resolve, reject) => {
+            const queryString = 'SELECT * FROM Users WHERE username=?';
+            db.query(queryString, [user.username], (err, res) => {
+                console.log(err, res, "hello");
+                if (err) {
+                    // send back an error
+                    console.log("found an error");
+                    reject(err);
+                } else {
+                    if (res.length) {
+                        // found a user with username that was passed in
+                        resolve(res[0]);
+                    } else {
+                        // did not find a user with username
+                        resolve(false);
+                    }
+                }
+            });
+        });
+    },
+    registerNewUser: function(user) {
+        console.log("User.js -> registerNewUser");
+        console.log(user);
+        //const queryString = 'INSERT INTO Users(username, password) VALUES (' + user.username + ', ' + user.pword + ')';
+        return new Promise((resolve, reject) => {
+           const queryString = "INSERT INTO Users(username,password) VALUES ('"+user.username+"',  '"+user.pword+"')";
+           //	const queryString = 'INSERT INTO Users(username, password) VALUES ('" + user.username + "', '" + user.pword + "')';
+            db.query(queryString, (err, res) => {
+                console.log(err, res, "hello");
+                if (err) {
+                    // send back an error
+                    console.log("found an error");
+                    reject(err);
+                } else {
+                    if (res.length) {
+                        // found a user with username that was passed in
+                        // console.log("Succesfully inserted");
+                        resolve(false);
+                        console.log(res);
+                        
+                    } else {
+                        // did not find a user with username
+                        // console.log("username taken");
+                        console.log("Succesfully inserted");
+                        resolve(res[0]);
+                    }
+                }
+            });
+        });
+
+
+
+
+
+
+        // return new Promise((resolve, reject) => {
+            
+        //     const queryString = 'SELECT * FROM Users WHERE username=?';
+        //     console.log("queryString ", queryString);
+        //     //		DatabaseConnection.query('INSERT INTO Air_Pollution_Record (Air_Pollution_Reading) VALUES ('+Pollution_Reading+')');
+
+        //     //INSERT INTO Users (username, password) VALUES("jassy", "polygloter");
+        //     db.query(queryString, (err, res) => {
+        //         console.log(err, res, "registerNewUser");
+        //     });
+        // });
+    },
 }
--- node_projects/chatapp-node/routes/user.js
+++ node_projects/chatapp-node/routes/user.js
@@ -48,33 +48,60 @@ user.get('/register', function(req, res, next) {
 user.post('/register', function(req, res, next) {
 	console.log("/register post")
   //expect username password
+ //  UserDB.findOne(req.body)
+ //  .then((user) => {
+	// // console.log("body", req.body)
+	// // console.log("found a user", user)
+	// // console.log("found a password", user.password)
+	// // console.log("html password: ", req.body.pword)
+	// console.log("reg user response: ", user);
+	
+	// if (user === false) {
+	// 	console.log("no user found");
+	// 	res.redirect('/user');
+	//  //  if (req.body.pword === user.password) {
+	// 	// // res.send(200, user);
+	// 	// res.redirect('/user');
+	//  //  } 
+	//  //  else {
+	// 	// // res.send(400, {err: 'Incorrect password'});
+	// 	// console("Created user")
+	//  //  }
+	// }
+	// else{
+	// 	console.log("username already exists");
+	// }
+ //  })
+ //  .catch((err) => {
+	// console.log("found an error", err);
+	// res.send(500, err);
+ //  });
+
   UserDB.findOne(req.body)
   .then((user) => {
 	// console.log("body", req.body)
 	// console.log("found a user", user)
 	// console.log("found a password", user.password)
 	// console.log("html password: ", req.body.pword)
-	console.log("reg user response: ", user);
+
+	// UserDB.registerNewUser(req.body).then((user) => {
+	// 	console.log("user.js -> registerNewUser ");
+ //  	});
+ 
 	if (user === false) {
-		console.log("no user found");
-		res.redirect('/user');
-	 //  if (req.body.pword === user.password) {
-		// // res.send(200, user);
-		// res.redirect('/user');
-	 //  } 
-	 //  else {
-		// // res.send(400, {err: 'Incorrect password'});
-		// console("Created user")
-	 //  }
+		UserDB.registerNewUser(req.body).then((user) => {
+			console.log("user.js -> registerNewUser ");
+  		});
 	}
 	else{
-		console.log("username already exists");
+		console.log("user already exists");
 	}
   })
   .catch((err) => {
 	console.log("found an error", err);
 	res.send(500, err);
   });
+
 });
 
 
